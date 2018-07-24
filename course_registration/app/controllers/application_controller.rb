2f317bf3a932e39d3e9a6a44640672bcb595a4a4
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-24 19:06:36 +0000

    Added teacher controller actions and helper methods for login actions

--- course_registration/app/controllers/application_controller.rb
+++ course_registration/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_teacher, :logged_in?
+
+  def current_teacher
+    @current_teacher ||= Teacher.find_by_id(session[:teacher_id])
+  end
+
+  def logged_in?
+    current_teacher != nil
+  end
 end
--- course_registration/app/controllers/teachers_controller.rb
+++ course_registration/app/controllers/teachers_controller.rb
@@ -1,15 +1,36 @@
 class TeachersController < ApplicationController
 
   def new
+    @teacher = Teacher.new
   end
 
   def create
+    @teacher = Teacher.new(teacher_params)
+    if @teacher.save
+      session[:teacher_id] = @teacher.id
+      redirect_to teacher_path(@teacher)
+    else
+      render '/'
+    end
   end
 
   def show
+    if logged_in?
+      @teacher = current_teacher
+    else
+      redirect_to '/'
+    end
   end
 
   def destroy
+    session.delete :teacher_id
+    redirect_to '/'
   end
-  
+
+  private
+
+  def teacher_params
+    params.require(:teacher).permit(:username, :password, :subject)
+  end
+
 end
