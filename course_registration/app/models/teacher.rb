288ebd830e48a25247e8bc3083b5a7e75108c4eb
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-25 19:17:13 +0000

    Added teacher validations

--- course_registration/app/models/teacher.rb
+++ course_registration/app/models/teacher.rb
@@ -1,6 +1,8 @@
 class Teacher < ActiveRecord::Base
   has_secure_password
-  
+  validates :username, presence: true
+  validates :username, uniqueness: true
+
   has_many :courses
   has_many :students, through: :courses
 end
