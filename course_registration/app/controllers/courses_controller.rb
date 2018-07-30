0ba16838053156da00e246d1bcb8b18fb668ddd1
Elle Wilson <eleanorwilsonn@gmail.com>
2018-07-30 15:43:28 +0000

    Updated Course views

--- course_registration/app/controllers/courses_controller.rb
+++ course_registration/app/controllers/courses_controller.rb
@@ -1,7 +1,20 @@
 class CoursesController < ApplicationController
+  def index
+    @courses = Course.all
+  end
+
+
   def create
   @course = Course.create(course_params)
   redirect_to teacher_path(@course.teacher)
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def show
+    @course = Course.find_by_id(params[:id])
   end
 
   private
--- course_registration/app/models/student.rb
+++ course_registration/app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < ActiveRecord::Base
   has_many :grades
   has_many :teachers, through: :grades
+  belongs_to :course
 end
